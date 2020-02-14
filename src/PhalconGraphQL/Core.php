f76b9caac84030c45d6ed247c7a85072a7f8428e
Bart Blok <bart@wittig.nl>
2020-02-14 16:54:55 +0100

    Added objectName option for classes

--- src/PhalconGraphQL/Core.php
+++ src/PhalconGraphQL/Core.php
@@ -6,6 +6,10 @@ class Core extends \PhalconApi\Core
 {
     public static function getShortClass($class){
 
+        if(method_exists($class, 'objectName')){
+            return $class::objectName();
+        }
+
         $path = explode('\\', $class);
         return array_pop($path);
     }
